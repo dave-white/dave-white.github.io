2e2472afc280c02b63a7d8076e523ae2314890df
David G. White <dave-white@users.noreply.github.com>
2022-02-05 13:21:08 -0500

    Backup

--- src/router.php
+++ src/router.php
@@ -1,6 +1,7 @@
 <?php
 if (php_sapi_name() == 'cli-server') {
-  $rqst_split_mask = '/(.*\/)((\w+)(\.\w+)?)$/';
+  $rout_dir = __DIR__;
+  $rqst_split_mask = '/\/([\w\-\/]*\/)?(([\w\-]+)(\.\w+)?)$/';
   if (preg_match($rqst_split_mask, $_SERVER["REQUEST_URI"], $matches, PREG_UNMATCHED_AS_NULL)) {
     $rqst_pieces = array(
       'uri' => $matches[0],
@@ -12,11 +13,27 @@ if (php_sapi_name() == 'cli-server') {
     if (trim($rqst_pieces['dir'], '/') > 0) {
       chdir(trim($rqst_pieces['dir'], '/'));
     }
+
+    $ret_file = '';
     if (strlen($rqst_pieces['resource_ext'] > 0)) {
-      include $rqst_pieces['resource'];
+      $ret_file = $rqst_pieces['resource'];
+    } elseif (strlen($rqst_pieces['resource_name']) > 0) {
+      $ret_file = $rqst_pieces['resource_name'].'.php';
     } else {
-      include $rqst_pieces['resource_name'].'.php';
+      $ret_file = 'index.php';
     }
+
+    if (file_exists($ret_file)) {
+      echo '1!';
+      var_dump($rqst_pieces);
+      include $ret_file;
+    } else {
+      echo '2!';
+      var_dump($rqst_pieces);
+      include __DIR__.'/index.php';
+    }
+  } else {
+    include 'index.php';
   }
 }
 ?>
